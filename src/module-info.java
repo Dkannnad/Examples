2b63b56174eed00c459f6488f062fa4cc32a7e27
LENOVO <LENOVO@Dina>
2023-07-12 21:22:01 +0530

    First Commit

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,24 @@
 module example {
-}
+	public class Maximum_Array {
+		
+		public static void main(String[] args) {
+			
+			int a[]= {10,20,50,25};
+			
+			int max=a[0];
+			
+			for (int i = 1; i < a.length; i++) {
+				
+				if (a[i]>max) {
+					
+					max=a[i];
+					
+				}
+				
+			}
+			System.out.println(max);
+			
+		}
+
+	}
+
